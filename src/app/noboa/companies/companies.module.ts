ca7a93c21a1876323f011b502bc5fe3e13b4a0a4
Brobbey <wirekobrobbey27@gmail.com>
2023-04-14 18:19:27 +0000

    Companies page navigation with bugs

--- src/app/noboa/companies/companies.module.ts
+++ src/app/noboa/companies/companies.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
+import { ComponentsPageModule } from 'src/app/components/components.module';
 import { CompaniesPageRoutingModule } from './companies-routing.module';
-
 import { CompaniesPage } from './companies.page';
-
+ 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    ComponentsPageModule,
     CompaniesPageRoutingModule
   ],
   declarations: [CompaniesPage]
